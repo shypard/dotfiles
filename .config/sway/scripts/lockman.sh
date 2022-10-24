185c01d5dc6d9f44fbddc30195d045167ae922c6
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2022-10-24 09:35:37 +0200

    Update application defaults & lockscreen

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-# Times the screen off and puts it to background
-swayidle \
-    timeout 5 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' &
-
-# Locks the screen immediately
-swaylock \
-    --screenshots \
+swaylock_options='--screenshots \
     --clock \
-    --effect-blur 7x5
+    --effect-blur 7x5 \
     --effect-vignette 0.5:0.5 \
     --grace 2 \
-	--fade-in 1
+    --fade-in 1'
+
+# Lock screen automatically after 5 minutes, then turn off after another 5mins.
+swayidle -w \
+    timeout 300 "swaylock -f $swaylock_options" \
+    timeout 600 'swaymsg "output * dpms off"' \\
+            resume 'swaymsg "output * dpms on"' \\
+            before-sleep "swaylock -f $swaylock_options"
+
+
+# Locks the screen immediately
+swaylock $swaylock_options
 
 # Kills last background task so idle timer doesn't keep running
 kill %%
