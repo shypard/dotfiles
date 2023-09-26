e207d5a008a0e70584cdb329b8ebb40d15d6878a
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-09-26 08:15:07 +0200

    wip

--- collect.sh
+++ collect.sh
@@ -1,22 +1,8 @@
 #/bin/bash
-# Collects all desired dotfiles and copies them to this git repository
 
-dotfiles=(
-    "$HOME/.config/zathura"
-    "$HOME/.config/alacritty"
-    "$HOME/.config/nvim"
-    "$HOME/.config/sway"
-    "$HOME/.config/waybar"
-    "$HOME/.config/ranger"
-    "$HOME/.config/swaylock"
-    "$HOME/.config/wofi"
-    "$HOME/.config/mako"
-    "$HOME/.config/aerc"
-)
+set -e
 
-# copy all files to this directory
-for f in "${dotfiles[@]}"; do
-    ls $f
+# copy all configured dotfiles to this repository
+for f in $(ls .config); do
     cp --recursive --update $f .config
 done
-
