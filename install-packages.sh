48f5eeb09687ea3caf768b01ff8593c935f2f40a
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-10-07 18:34:57 +0200

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -8,8 +8,8 @@ sudo dnf copr enable -y \
 # installing login- and window manager
 sudo dnf install -y \
     greetd-tuigreet \
-    sway
-#    swayfx \
+    seatd \
+    swayfx
 
 # installing user applications
 sudo dnf install -y \
@@ -25,11 +25,25 @@ sudo cat > /etc/greetd/config.toml << EOF
 vt = "current"
 
 [default_session]
-command = "tuigreet --cmd sway -w 40 -g 'Log in' -r -t --asterisks --power-shutdown 'sudo systemctl poweroff'"
-user = "greeter"
+user = "greetd"
+command = "tuigreet \
+    --cmd sway \
+    -w 40 \
+    -g 'Log in' \
+    -r \
+    -t \
+    --asterisks \
+    --power-shutdown 'sudo systemctl poweroff'"
 EOF
 
+# enable greetd and seatd services
 sudo systemctl set-default graphical.target
 sudo systemctl enable --now greetd.service
+sudo systemctl enable --now seatd.service
+
+# add user to seat group
+sudo usermod -a -G seat $(whoami)
+
+
 
 echo "Done"
