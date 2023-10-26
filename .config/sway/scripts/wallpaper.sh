397a60dc63b77ccc77593cb4a5d763b6a9f2660f
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-10-26 16:04:56 +0200

    Add sway & swaylock config

---
+++ .config/sway/scripts/wallpaper.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+pkill swaybg
+
+swaybg -o 'Philips Consumer Electronics Company PHL 241B8Q ZV02203007229' -i ~/images/bg/mountain-1.jpg -m fill &
+swaybg -o 'Philips Consumer Electronics Company PHL 241B8Q ZV02203007227' -i ~/images/bg/mountain-2.jpg -m fill &
+swaybg -o 'eDP-1' -i ~/Pictures/seaowl_wallpaper.png -m fill &
