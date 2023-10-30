4bd4b058ebc787bd49b5952925477c2f175bc852
Christian <31901336+shypard@users.noreply.github.com>
2023-10-30 16:57:58 +0000

    Update package installer script

--- install-packages.sh
+++ install-packages.sh
@@ -16,26 +16,25 @@ sudo dnf install -y \
     git
 
 # configure tui-greet
-sudo cat > /etc/greetd/config.toml << EOF
+sudo cat << EOF | sudo tee /etc/greetd/config.toml
 [terminal]
 vt = "current"
 
 [default_session]
 user = "greetd"
-command = "tuigreet \
-    --cmd sway \
-    -w 40 \
-    -g 'Log in' \
-    -r \
-    -t \
-    --asterisks \
-    --power-shutdown 'sudo systemctl poweroff'"
+command = "tuigreet --cmd sway -w 40 -g 'Log in' -r -t --asterisks --power-shutdown 'sudo systemctl poweroff'"
 EOF
 
-# enable greetd and seatd services
+# remove unused services and tools
+sudo dnf remove -y \
+    sddm \
+    foot
+
+# enable greetd, seatd and ssh server
 sudo systemctl set-default graphical.target
 sudo systemctl enable --now greetd.service
 sudo systemctl enable --now seatd.service
+sudo systemctl enable --now sshd.service
 
 # add user to seat group
 sudo usermod -a -G seat $(whoami)
