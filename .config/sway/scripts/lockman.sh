cfb571376871463f1a9435cfa1fe177222d64583
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-11-07 14:36:21 +0100

    Update dotfiles

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -2,14 +2,14 @@
 
 # Lock screen automatically after 5 minutes, then turn off after another 5mins.
 swayidle -w \
-    timeout 300 "swaylock -f" \
+    timeout 300 "swaylock -f -c 000000" \
     timeout 1200 'swaymsg "output * dpms off"' \\
             resume 'swaymsg "output * dpms on"' \\
-            before-sleep "swaylock -f"
+            before-sleep "swaylock -f -c 000000"
 
 
 # Locks the screen immediately
-swaylock -c 00000
+swaylock -c 000000
 
 # Kills last background task so idle timer doesn't keep running
 kill %
