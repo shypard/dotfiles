2e3a99767fb5f8108c9c2bbc97468a23b7f3408f
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-11-16 23:22:56 +0100

    Adapt swaybg script

--- .config/sway/scripts/wallpaper.sh
+++ .config/sway/scripts/wallpaper.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 pkill swaybg
 
-swaybg -o 'DP-2' -i ~/images/bg/mountain-1.jpg -m fill &
-swaybg -o 'DP-3' -i ~/images/bg/mountain-2.jpg -m fill &
+swaybg -i $HOME/.config/sway/assets/bg/forest.jpg &
