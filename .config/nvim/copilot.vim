4ec3ce85d488c35cc99b5203ddc2abda8e0b25d9
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2023-11-24 10:00:12 +0100

    Update

--- .config/nvim/copilot.vim
+++ .config/nvim/copilot.vim
@@ -1,7 +1,7 @@
 " $HOME/.config/nvim/copilot.vim
 
 " disable tab mapping
-let g:copilot_no_tab_map = 1 
+" let g:copilot_no_tab_map = 1 
 
 "use <Tab><Tab> to accept suggestion --> use something else
 " imap <silent><script><expr> <Tab><Tab> copilot#Accept('\<CR>')
--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-# Lock screen automatically after 5 minutes, then turn off after another 5mins.
-swayidle -w \
-    timeout 300 "swaylock -f -c 000000" \
-    timeout 1200 'swaymsg "output * dpms off"' \\
-            resume 'swaymsg "output * dpms on"' \\
-            before-sleep "swaylock -f -c 000000"
-
-
 # Locks the screen immediately
-swaylock -c 000000
+swaylock -f
 
 # Kills last background task so idle timer doesn't keep running
 kill %
