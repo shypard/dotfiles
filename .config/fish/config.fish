737a31793bd5569123a72df49767e512d344cce9
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2024-01-06 16:04:17 +0100

    Update fish & sway config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
+# disable fish greeter
 set fish_greeting
+
+# set paths
+set fish_user_paths /home/cs/.local/scripts
+set fish_user_paths /home/cs/.local/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+
