a57cf670f2c2b311df4a6ca93de5b35c532d8d63
Christian Schaefer <christian.schaefer@fh-salzburg.ac.at>
2024-01-24 15:40:52 +0100

    update to alacritty toml config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,9 @@
 set fish_greeting
 
 # set paths
-set fish_user_paths /home/cs/.local/scripts
-set fish_user_paths /home/cs/.local/bin
+set fish_add_path /home/cs/.local/scripts
+set fish_add_path /home/cs/.local/bin
+set fish_add_path /home/cs/.cargo/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
